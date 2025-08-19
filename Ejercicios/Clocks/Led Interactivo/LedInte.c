9d8a3d5580867118047a4eeeb7b3d7c4c5ce2cd4
Tomas Daniel <163681220+TomyDaniel@users.noreply.github.com>
2025-08-19 12:30:30 -0300

    Fix LED and button definitions; update main loop for LED control and delay function

--- Ejercicios/Clocks/Led Interactivo/LedInte.c
+++ Ejercicios/Clocks/Led Interactivo/LedInte.c
@@ -6,7 +6,8 @@
 
 #include <cr_section_macros.h>
 
-#define LED1 ()
+#define LED1 (1 << 22)
+#define B_PUSH (1 << 10)
 
 void delay(int cantidad);
 
@@ -19,7 +20,14 @@ int main(void) {
 
     while(1) {
 
-
+        if((LPC_GPIO2->FIOPIN & B_PUSH) == 0){
+            LPC_GPIO0->FIOSET = LED1;
+        }else{
+            LPC_GPIO0->FIOSET = LED1;
+            delay(500);
+            LPC_GPIO0->FIOCLR = LED1;
+            delay(500);
+        }
         
     }
 
@@ -27,11 +35,28 @@ int main(void) {
 }
 
 void delay(int cantidad){
-	
+	int ticks =(SystemCoreClock / 1000) * cantidad;
+
+    SysTick->LOAD = ticks - 1;
+
+    SysTick->VAL = 0;
+
+    SysTick->CTRL = (1 << 2) | (1 << 0);
+
+    while(!(SysTick->CTRL & (1 << 16)));
+
+    SysTick->CTRL = 0;
 }
 
 
 void configuraciones(void){
 
+    LPC_PINCON->PINSEL1 &= ~(3 << 12);
+
+    LPC_GPIO0->FIODIR |= LED1;
+
+    LPC_PINCON->PINSEL4 &= ~(3 << 20);
+
+    LPC_GPIO2->FIODIR &= ~B_PUSH;
 
 }
